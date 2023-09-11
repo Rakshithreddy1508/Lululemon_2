eca6260b25b3477fe3a2b9468b472ae813c8cec7
rakshithreddy1508.com <rakshithreddy17cv.rymec@gmail.com>
2023-09-11 15:51:33 +0530

    first commit rakshith

---
+++ src/Encapsulation/Helloworld.java
@@ -0,0 +1,10 @@
+package Encapsulation;
+
+public class Helloworld {
+	public static void main(String[] args) {
+	System.out.println("We are trying to explore to Git");
+	System.out.println("We also explore what is local and Remote repo");
+	System.out.println("We also explore what is GitBash and Git Hub");
+	}
+
+}
